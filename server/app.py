be83fc2322b1beac61797903e96a0a23035f32a7
tyranorah <tashnorah@gmail.com>
2024-07-10 04:07:59 +0300

    initial1

--- server/app.py
+++ server/app.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 
-# Standard library imports
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from config import Config
+from models import db, User, Product, Order, OrderItem
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+app.config.from_object(Config)
 
+db.init_app(app)
 
-# Views go here!
+with app.app_context():
+    db.create_all()
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(name=data['name'], email=data['email'], gender=data['gender'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
 
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify(user.serialize())
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-
+    app.run(debug=True)
